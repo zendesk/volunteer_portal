aad95665e6202c0d4a20fe77dca9689bb12095a1
Xuebing Li <xuebinglee@gmail.com>
2018-09-26 16:50:35 +1000

    Add 24 hours limit to individual event duration

--- app/models/individual_event.rb
+++ app/models/individual_event.rb
@@ -12,7 +12,9 @@ class IndividualEvent < ApplicationRecord
 
   validates :user, :organization, :event_type, :office,
             :description, :date, presence: true
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+  validates :duration, numericality: { only_integer: true,
+                                       greater_than: 0,
+                                       less_than_or_equal_to: 1440 }
 
   scope :for_user, ->(user) { where(user: user) }
   scope :before,   ->(date) { where("date < ?", date) }
