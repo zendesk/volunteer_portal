24bdc459c340fdef07644ff07f6d27e9226b8233
Daniel Ribeiro <djbreves@gmail.com>
2018-12-21 10:57:16 +1100

    Reinstate missing helper

--- app/javascript/lib/utils.js
+++ app/javascript/lib/utils.js
@@ -1,4 +1,6 @@
 import R from 'ramda'
 
+export const present = v => !R.isNil(v) && !R.isEmpty(v)
+
 export const defaultFilterMethod = (filter, row, column) =>
   R.contains(filter.value.toLowerCase(), String(row[filter.id]).toLowerCase())
