9f45afddc1928b3bb9af1a308e2465a50e4e0717
Sam Xie <samxie@outlook.com.au>
2019-06-19 13:06:05 +1000

    update copy

--- app/javascript/pages/MyEvents/index.js
+++ app/javascript/pages/MyEvents/index.js
@@ -351,7 +351,7 @@ const IndividualEvents = props => {
           </button>
         </div>
         <h1>Individual Events</h1>
-        <h4>Attended events organized by you.</h4>
+        <h4>Events organized by you.</h4>
       </div>
       <ReactTable
         NoDataComponent={() => null}
@@ -420,7 +420,7 @@ const organizedEventsColumns = [
 const OrganizedEvents = ({ currentUser: { signups } }) => (
   <div className={s.eventsTable}>
     <h1>Organized Events</h1>
-    <h4>Attended events organized by your organization.</h4>
+    <h4>Events organized by your organization.</h4>
     <ReactTable
       NoDataComponent={() => null}
       data={signups.map(signup => signup.event)}
