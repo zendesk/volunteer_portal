6475d1805ac93acb4c0b96202744fddbf739acb7
Kenneth Chai <kchai@zendesk.com>
2019-08-14 16:25:11 +1000

    Complete the all method

--- app/graphql/resolvers/top_volunteer_resolver.rb
+++ app/graphql/resolvers/top_volunteer_resolver.rb
@@ -14,6 +14,15 @@ module TopVolunteerResolver
                   end
 
       events_scope, individual_events_scope = scope_with_time(events_scope, individual_events_scope, args[:after], args[:before])
+      events_scope, individual_events_scope = scope_with_office_id(events_scope, individual_events_scope, office_id)
+
+      all_events_scope = events_scope.union_all(individual_events_scope)
+      all_events_scope = all_events_scope.select('id, SUM(duration) AS duration').group('id')
+
+      all_events_scope = scope_with_count(all_events_scope, args[:count])
+      all_events_scope = scope_with_sort_by(all_events_scope, args[:sort_by])
+
+      all_events_scope
     end
 
     private
