fedabed83836acbbbc81f80517262826278a1905
Thu Trinh <thu.hm.trinh@gmail.com>
2020-01-12 16:23:54 +1100

    :heavy_plus_sign: Add apollo hooks and garden grids
    Apollo Hooks can live alongside the old ApolloProvider. We want to start replacing the existting apollo queries and mutations with the new hooks implementation. It is a much more terse interface. Hopefully this can also help us with moving away from redux.

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -5,6 +5,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { ThemeProvider } from '@zendeskgarden/react-theming'
@@ -26,9 +27,11 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <ApolloProvider store={store} client={client}>
-    <ThemeProvider>
-      <Root store={store} history={history} />
-    </ThemeProvider>
+    <ApolloHooksProvider client={client}>
+      <ThemeProvider>
+        <Root store={store} history={history} />
+      </ThemeProvider>
+    </ApolloHooksProvider>
   </ApolloProvider>,
   document.getElementById('root')
 )
