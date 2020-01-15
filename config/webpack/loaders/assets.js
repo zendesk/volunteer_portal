66896ccc8a72f1c1ea13d371f2dcacd8f755dc43
Thu Trinh <thu.hm.trinh@gmail.com>
2020-01-15 21:53:54 +1100

    :recycle: Add loader for Garden svg-icons
    Webpack properly loads in svgs from Garden

--- config/webpack/loaders/assets.js
+++ config/webpack/loaders/assets.js
@@ -2,11 +2,14 @@ const { env, publicPath } = require('../configuration.js')
 
 module.exports = {
   test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
-  use: [{
-    loader: 'file-loader',
-    options: {
-      publicPath,
-      name: env.NODE_ENV === 'production' ? '[name]-[hash].[ext]' : '[name].[ext]'
-    }
-  }]
+  use: [
+    {
+      loader: 'file-loader',
+      options: {
+        publicPath,
+        name: env.NODE_ENV === 'production' ? '[name]-[hash].[ext]' : '[name].[ext]',
+      },
+    },
+  ],
+  exclude: /@zendeskgarden/,
 }
