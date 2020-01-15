2d673f96d4969621de7788a068a8343f5603b32e
Thu Trinh <thu.hm.trinh@gmail.com>
2020-01-15 14:31:58 +1100

    :ok_hand: Switch from welcome notification to modal
    In this case the modal makes more sense as we want the user to actively confirm they are happy with the assigned default office

--- app/javascript/components/App/index.js
+++ app/javascript/components/App/index.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { ThemeProvider as GardenThemeProvider } from '@zendeskgarden/react-theming'
 
 import UserContext from '../../context/UserContext'
 import Loading from 'components/LoadingIcon'
 import Header from 'components/Header'
-import Notifications from 'components/Notifications'
+import WelcomeModal from 'components/WelcomeModal'
+
+import '@zendeskgarden/react-modals/dist/styles.css'
+import '@zendeskgarden/react-buttons/dist/styles.css'
+import '@zendeskgarden/react-tags/dist/styles.css'
 
 const MuiTheme = {
   fontFamily:
@@ -19,23 +24,25 @@ const MuiTheme = {
 
 const App = ({ loading, currentUser, offices, userPopover, toggleUserPopover, updateUserOffice, children }) => (
   <UserContext.Provider value={currentUser}>
-    <MuiThemeProvider muiTheme={getMuiTheme(MuiTheme)}>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div>
-          <Header
-            currentUser={currentUser}
-            offices={offices}
-            togglePopover={e => toggleUserPopover(e.currentTarget)}
-            popover={userPopover}
-            handleOfficeSelect={office => updateUserOffice(currentUser, office)}
-          />
-          {currentUser && <Notifications />}
-          {children}
-        </div>
-      )}
-    </MuiThemeProvider>
+    <GardenThemeProvider>
+      <MuiThemeProvider muiTheme={getMuiTheme(MuiTheme)}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <Header
+              currentUser={currentUser}
+              offices={offices}
+              togglePopover={e => toggleUserPopover(e.currentTarget)}
+              popover={userPopover}
+              handleOfficeSelect={office => updateUserOffice(currentUser, office)}
+            />
+            <WelcomeModal />
+            {children}
+          </div>
+        )}
+      </MuiThemeProvider>
+    </GardenThemeProvider>
   </UserContext.Provider>
 )
 
---
+++ app/javascript/components/WelcomeModal/index.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react'
+
+import * as R from 'ramda'
+import { UserContext } from '../../context'
+import { useMutation } from '@apollo/react-hooks'
+import { Modal, Header, Body, Footer, FooterItem } from '@zendeskgarden/react-modals'
+import { Button } from '@zendeskgarden/react-buttons'
+import { Tag } from '@zendeskgarden/react-tags'
+
+import ConfirmProfileSettingsMutation from './mutations/confirmProfileSettings.gql'
+
+const isFirstSignIn = R.propSatisfies(R.isNil, 'lastSignInAt')
+const confirmedProfileSetting = R.complement(R.path(['preference', 'confirmedProfileSettings']))
+const requireConfirmProfileSettings = R.allPass([isFirstSignIn, confirmedProfileSetting])
+
+const Notifications = () => {
+  const currentUser = useContext(UserContext)
+  const [confirmProfileSettings, _] = useMutation(ConfirmProfileSettingsMutation)
+
+  const officeName = R.path(['office', 'name'])
+  const dismiss = () => {
+    confirmProfileSettings()
+  }
+
+  if (currentUser && requireConfirmProfileSettings(currentUser)) {
+    return (
+      <Modal>
+        <Header> 🎉 Welcome {currentUser.name}</Header>
+        <Body>
+          <p>
+            We have chosen <strong>{officeName(currentUser)}</strong> as your current office.
+          </p>
+          <p>
+            To choose a different office, simply use the <Tag pill>Default Office</Tag> setting located in your profile
+            menu (top right corner of your screen.)
+          </p>
+        </Body>
+        <Footer>
+          <FooterItem>
+            <Button primary onClick={dismiss}>
+              Got it!
+            </Button>
+          </FooterItem>
+        </Footer>
+      </Modal>
+    )
+  }
+
+  return null
+}
+
+export default Notifications
