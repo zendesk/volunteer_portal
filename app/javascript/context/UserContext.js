9631e99c47150c5f6011fced53d9fd112d6fa4e1
Thu Trinh <thu.hm.trinh@gmail.com>
2020-02-14 15:38:38 +1100

    :bug: Welcome modal now closes when the "git it" button is pressed
    git push

--- app/javascript/context/UserContext.js
+++ app/javascript/context/UserContext.js
@@ -2,17 +2,22 @@ import React, { useState, useCallback } from 'react'
 
 import * as R from 'ramda'
 
+const preferenceLens = R.lensProp('preference')
+const officeLens = R.lensProp('office')
+
 export const UserContext = React.createContext(null)
 
 export const UserContextProvider = ({ user, children }) => {
   const [currentUser, setCurrentUser] = useState(user)
 
-  const setOffice = useCallback(office => setCurrentUser(R.set(R.lensProp('office'), office)), [])
+  const setOffice = useCallback(office => setCurrentUser(R.set(officeLens, office)), [])
+  const setPreference = useCallback(pref => setCurrentUser(R.set(preferenceLens, pref)), [])
   const clear = useCallback(() => setCurrentUser(undefined), [])
 
   const ctx = {
     currentUser,
     setOffice,
+    setPreference,
     clear,
   }
 
