0a1dafaedafadfeff3dfab2e20b6cfae897b4f46
Jing Li <jingli.claire@gmail.com>
2020-03-11 15:47:41 +1100

    :recycle: Refactor using Apollo Hooks for tagMutation query

--- app/javascript/pages/admin/Tags/edit.js
+++ app/javascript/pages/admin/Tags/edit.js
@@ -1,28 +1,40 @@
-import React from 'react'
-import { graphql, compose } from 'react-apollo'
-import { useQuery } from '@apollo/react-hooks'
-import { NetworkStatus } from 'apollo-client'
-import { connect } from 'react-redux'
 import * as R from 'ramda'
+import React, { useState } from 'react'
 
-import { graphQLError } from 'actions'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 
-import TagForm from './form'
 import Loading from 'components/LoadingIcon'
-
+import TagForm from './form'
 import TagQuery from './queries/show.gql'
 import UpdateTagMutation from './mutations/update.gql'
 
 const EditTag = props => {
-  const { updateTag } = props
+  const [updateError, setUpdateError] = useState(null)
   const { data, loading, error } = useQuery(TagQuery, {
     variables: { id: props.params.id },
   })
+  const [mutate, _] = useMutation(UpdateTagMutation)
+  const updateTag = tag => {
+    mutate({
+      variables: { input: R.omit(['__typename'], tag) },
+      optimisticResponse: buildOptimisticResponse(tag),
+    })
+      .then(_response => {
+        props.router.push('/portal/admin/tags')
+      })
+      .catch(({ graphQLErrors }) => {
+        setUpdateError(graphQLErrors)
+      })
+  }
 
   if (loading) return <Loading />
   if (error) return <div>Tag Not Found</div>
-
-  return <TagForm tag={data.tag} onSubmit={updateTag} />
+  return (
+    <>
+      {updateError && <p>Unable to update the tags</p>}
+      <TagForm tag={data.tag} onSubmit={updateTag} />
+    </>
+  )
 }
 
 const buildOptimisticResponse = tag => ({
@@ -33,31 +45,4 @@ const buildOptimisticResponse = tag => ({
   },
 })
 
-const withData = compose(
-  graphql(UpdateTagMutation, {
-    props: ({ ownProps, mutate }) => ({
-      updateTag: tag =>
-        mutate({
-          variables: { input: R.omit(['__typename'], tag) },
-          optimisticResponse: buildOptimisticResponse(tag),
-        })
-          .then(_response => {
-            ownProps.router.push('/portal/admin/tags')
-          })
-          .catch(({ graphQLErrors }) => {
-            ownProps.graphQLError('tag', graphQLErrors)
-          }),
-    }),
-  })
-)
-
-const mapStateToProps = (state, ownProps) => ({})
-
-const withActions = connect(
-  mapStateToProps,
-  {
-    graphQLError,
-  }
-)
-
-export default withActions(withData(EditTag))
+export default EditTag
