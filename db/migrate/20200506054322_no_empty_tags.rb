222a15f212d4376f4a78b1eb2e02a60d47b805b6
Sam Xie <sxie@zendesk.com>
2020-05-06 16:05:45 +1000

    rubocop

--- db/migrate/20200506054322_no_empty_tags.rb
+++ db/migrate/20200506054322_no_empty_tags.rb
@@ -1,9 +1,9 @@
 class NoEmptyTags < ActiveRecord::Migration[5.1]
   def change
-    if Tag.all.length == 0
-      [
-        'New Hire', 'Skilled', 'Sustainability'
-      ].each { |tag| Tag.find_or_create_by(name: tag) }
-    end
+    return unless Tag.all.length.zero?
+
+    [
+      'New Hire', 'Skilled', 'Sustainability'
+    ].each { |tag| Tag.find_or_create_by(name: tag) }
   end
 end
