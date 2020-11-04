41a686f70c7b6949c201d60be556690bad5c4cba
Sam Xie <sxie@zendesk.com>
2020-11-04 12:29:43 +1100

    use named export after upgrade

--- app/javascript/store/configureStore.dev.js
+++ app/javascript/store/configureStore.dev.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import createRootReducer from '../reducers'
 
 export default function configureStore(apolloClient, initialState = {}) {
