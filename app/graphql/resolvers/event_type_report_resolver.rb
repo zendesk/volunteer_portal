dc8b9c05acba05abb7f26a6050dd322490860de0
Sam Xie <sxie@zendesk.com>
2020-12-02 15:40:17 +1100

    use heredoc syntax

--- app/graphql/resolvers/event_type_report_resolver.rb
+++ app/graphql/resolvers/event_type_report_resolver.rb
@@ -4,18 +4,20 @@ module EventTypeReportResolver
   class << self
     def individual(_object, args, _context)
       office_id = args[:office_id]
+      and_office = office_id == 'all' ? '' : "AND individual_events.office_id=#{office_id}"
       # We use UTC as the reports should be consistent for all admins regardless of time zones.
-      sql = "
-      SELECT \"id\", \"title\", \"sum\" from event_types INNER JOIN
+      sql = <<-SQL
+      SELECT "id", "title", "sum" from event_types INNER JOIN
         (SELECT SUM(duration), event_type_id
         from individual_events INNER JOIN event_types ON individual_events.event_type_id=event_types.id
-        WHERE \"individual_events\".\"deleted_at\" IS NULL
-        #{office_id == 'all' ? '' : "AND individual_events.office_id=#{office_id}"}
-        AND \"individual_events\".\"status\" = 1
+        WHERE "individual_events"."deleted_at" IS NULL
+        #{and_office}
+        AND "individual_events"."status" = 1
         AND (date > '#{Time.at(args[:after]).utc}')
         AND (date < '#{Time.at(args[:before]).utc}')
         GROUP BY event_type_id) as eventDuration
-      ON eventDuration.event_type_id=event_types.id"
+      ON eventDuration.event_type_id=event_types.id
+      SQL
 
       records_array = ActiveRecord::Base.connection.execute(sql)
       result = records_array.map do |record|
@@ -30,16 +32,18 @@ module EventTypeReportResolver
 
     def organized(_object, args, _context)
       office_id = args[:office_id]
-      sql = "
-      SELECT \"id\", \"title\", \"sum\" from event_types INNER JOIN
+      and_office = office_id == 'all' ? '' : "AND individual_events.office_id=#{office_id}"
+      sql = <<-SQL
+      SELECT "id", "title", "sum" from event_types INNER JOIN
         (SELECT SUM(duration), event_type_id
         from events LEFT JOIN event_types ON (events.event_type_id=event_types.id) LEFT JOIN signups ON signups.event_id = events.id
-        WHERE \"events\".\"deleted_at\" IS NULL
-        #{office_id == 'all' ? '' : "AND events.office_id=#{office_id}"}
+        WHERE "events"."deleted_at" IS NULL
+        #{and_office}
         AND (starts_at > '#{Time.at(args[:after]).utc}')
         AND (starts_at < '#{Time.at(args[:before]).utc}')
         GROUP BY event_type_id) as eventDuration
-      ON event_types.id=eventDuration.event_type_id"
+      ON event_types.id=eventDuration.event_type_id
+      SQL
 
       records_array = ActiveRecord::Base.connection.execute(sql)
       result = records_array.map do |record|
--- app/graphql/resolvers/tag_report_resolver.rb
+++ app/graphql/resolvers/tag_report_resolver.rb
@@ -4,19 +4,21 @@ module TagReportResolver
   class << self
     def individual(_object, args, _context)
       office_id = args[:office_id]
+      and_office = office_id == 'all' ? '' : "AND individual_events.office_id=#{office_id}"
       # We use UTC as the reports should be consistent for all admins regardless of time zones.
-      sql = "
-      SELECT \"id\", \"name\", \"sum\" from tags INNER JOIN
+      sql = <<-SQL
+      SELECT "id", "name", "sum" from tags INNER JOIN
         (SELECT SUM(duration), tag_id
         from individual_events INNER JOIN individual_event_tags ON individual_events.id=individual_event_tags.individual_event_id
-        WHERE \"individual_events\".\"deleted_at\" IS NULL
-        #{office_id == 'all' ? '' : "AND individual_events.office_id=#{office_id}"}
-        AND \"individual_events\".\"status\" = 1
+        WHERE "individual_events"."deleted_at" IS NULL
+        #{and_office}
+        AND "individual_events"."status" = 1
         AND (date > '#{Time.at(args[:after]).utc}')
         AND (date < '#{Time.at(args[:before]).utc}')
         GROUP BY tag_id) as tagDuration
       ON tagDuration.tag_id=tags.id
-      "
+      SQL
+
       records_array = ActiveRecord::Base.connection.execute(sql)
       result = records_array.map do |record|
         {
@@ -30,17 +32,18 @@ module TagReportResolver
 
     def organized(_object, args, _context)
       office_id = args[:office_id]
-      sql = "
-      SELECT \"id\", \"name\", \"sum\" from tags INNER JOIN
+      and_office = office_id == 'all' ? '' : "AND events.office_id=#{office_id}"
+      sql = <<-SQL
+      SELECT "id", "name", "sum" from tags INNER JOIN
         (SELECT SUM(duration), tag_id
         from events LEFT JOIN event_tags ON (events.id=event_tags.event_id) LEFT JOIN signups ON signups.event_id = events.id
-        WHERE \"events\".\"deleted_at\" IS NULL
-        #{office_id == 'all' ? '' : "AND events.office_id=#{office_id}"}
+        WHERE "events"."deleted_at" IS NULL
+        #{and_office}
         AND (starts_at > '#{Time.at(args[:after]).utc}')
         AND (starts_at < '#{Time.at(args[:before]).utc}')
         GROUP BY tag_id) as tagDuration
       ON tagDuration.tag_id=tags.id
-      "
+      SQL
 
       records_array = ActiveRecord::Base.connection.execute(sql)
       result = records_array.map do |record|
