cb0a91c4cd205568fc6df3c02c9c4460f7f67b2c
Kirsten McKenna <kayison@users.noreply.github.com>
2021-01-27 15:52:53 +1100

    Update LoadingIcon to use Garden Spinner

--- app/javascript/components/LoadingIcon.js
+++ app/javascript/components/LoadingIcon.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import * as R from 'ramda'
-import CircularProgress from 'material-ui/CircularProgress'
+import { Spinner } from '@zendeskgarden/react-loaders'
 
 const ownStyles = {
   loading: {
-    left: '50%',
-    marginLeft: -60,
-    stroke: '#30aabc',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
   },
 }
 
-const LoadingIcon = ({ style }) => (
-  <CircularProgress size={120} thickness={5} color="30aabc" style={R.merge(ownStyles.loading, style)} />
-)
+const LoadingIcon = ({ style }) => <Spinner size="120" color={'#30aabc'} style={R.merge(ownStyles.loading, style)} />
 
 export default LoadingIcon
