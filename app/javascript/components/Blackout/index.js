fc5cec8923879d85084a967511659f75942a499c
Kerri Sim <ksim@zendesk.com>
2021-01-27 15:49:04 +1100

    Change EventPopover to use garden components

--- app/javascript/components/Blackout/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import * as R from 'ramda'
-
-import s from './main.css'
-
-const Blackout = ({ width }) => <span className={s.blackout}>{'W'.repeat(width)}</span>
-
-export default Blackout
