a842d24aa57372a60a4b9e2088e7a749912b809e
Thu Trinh <thu.hm.trinh@gmail.com>
2021-03-10 16:41:53 +1100

    postgres in docker compatability: use TCP for postgres connection locally and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506054322) do
+ActiveRecord::Schema.define(version: 2020_05_06_054322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
