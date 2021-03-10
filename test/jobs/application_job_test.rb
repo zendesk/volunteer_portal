98d4f61843e2749270cb4b7baf7d1f32b0d7acce
Kirsten McKenna <kayison@users.noreply.github.com>
2021-03-10 13:47:40 +1100

    Ignore Lint/EmptyBlock rule in empty test files (singlecov)

--- test/jobs/application_job_test.rb
+++ test/jobs/application_job_test.rb
@@ -2,5 +2,7 @@ require_relative '../test_helper'
 
 SingleCov.covered!
 
+# rubocop:disable Lint/EmptyBlock
 describe ApplicationJob do
 end
+# rubocop:enable Lint/EmptyBlock
--- test/models/application_record_test.rb
+++ test/models/application_record_test.rb
@@ -2,5 +2,7 @@ require_relative '../test_helper'
 
 SingleCov.covered!
 
+# rubocop:disable Lint/EmptyBlock
 describe ApplicationRecord do
 end
+# rubocop:enable Lint/EmptyBlock
